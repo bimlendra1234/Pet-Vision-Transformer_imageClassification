fde2ac59b46159f637164ab129468571c600345d
Bimlendra Yadav <bimlendrayadav311@gmail.com>
2025-07-05 04:02:48 -0400

    version and app change

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 import gradio as gr
 import torch
-from PIL import Image
 from transformers import ViTForImageClassification, AutoImageProcessor
 
-MODEL_DIR = "vit_cats_dogs_final_model_20250330_231537"
+MODEL_DIR = "./vit_cats_dogs_final_model_20250330_231537"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Load model and processor
 model = ViTForImageClassification.from_pretrained(MODEL_DIR).to(device)
 processor = AutoImageProcessor.from_pretrained(MODEL_DIR)
+
 id2label = model.config.id2label
 
 def classify_image(image):
     inputs = processor(images=image, return_tensors="pt")
-    inputs = {k: v.to(device) for k, v in inputs.items()}  # ✅ FIXED
+    inputs = {k: v.to(device) for k, v in inputs.items()}
     outputs = model(**inputs)
     probs = torch.nn.functional.softmax(outputs.logits, dim=1)[0].cpu().numpy()
-    result = {id2label[i]: float(probs[i]) for i in range(len(probs))}
-    return result
+    return {id2label[str(i)]: float(probs[i]) for i in range(len(probs))}
 
 gr.Interface(
     fn=classify_image,
